7eabe07fe256c0e7aa9b9c6275fadccbe94fa2fc
Ivan Bütler <ivan.buetler@hacking-lab.com>
2025-06-20 03:47:09 -0400

    moved

--- start_openvpn.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Start VPN in terminal as root (debugging purpose)
-echo "==============================================================================="
-echo "   VPN WILL ONLY WORK IF YOU HAVE PREVIOUSLY AUTHENTICATED IN A HL WEB APP    "
-echo "==============================================================================="
-
-sudo /usr/sbin/openvpn --cd $PWD --script-security 2 --config $PWD/Hacking-Lab-2.0.ovpn
-
